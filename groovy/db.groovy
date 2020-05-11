a252988b9f9cd89561c956c4a644b7b2f3d28885
lhj <3093024668@qq.com>
2020-05-11 16:15:24 +0800

    调整Groovy的代码样式

--- groovy/db.groovy
+++ groovy/db.groovy
@@ -1,20 +1,21 @@
 import groovy.json.JsonOutput
 
-class DB{
-    static void main(String[] args){
-        checkDatabase()
+class DB {
+  static void main(String[] args) {
+    checkDatabase()
+  }
+
+  static void checkDatabase() {
+    def sql = groovy.sql.Sql.newInstance('jdbc:mysql://localhost:3306/demo?useUnicode=true&serverTimezone=GMT%2B8&characterEncoding=UTF-8&useSSL=false', 'root', 'lzw719', 'com.mysql.cj.jdbc.Driver')
+    //println sql.connection.catalog
+    def rows = sql.rows("select * from t_group")
+    println(rows)
+    def json = JsonOutput.toJson(rows)
+    println(json)
+    sql.eachRow("select * from t_group") {
+      tp ->
+        println([tp.id, tp.name])
     }
-    static void checkDatabase(){
-        def sql = groovy.sql.Sql.newInstance('jdbc:mysql://localhost:3306/xorm?useUnicode=true&serverTimezone=GMT%2B8&characterEncoding=UTF-8&useSSL=false', 'root', 'lzw719', 'com.mysql.jdbc.Driver')
-        //println sql.connection.catalog
-        def rows = sql.rows("select * from group_entity")
-        println(rows)
-        def json = JsonOutput.toJson(rows)
-        println(json)
-        sql.eachRow("select * from group_entity") {
-            tp ->
-                println([tp.id, tp.name])
-        }
-        sql.close()
-    }
+    sql.close()
+  }
 }
--- src/test/java/com/example/jsr223example/Jsr223exampleApplicationTests.java
+++ src/test/java/com/example/jsr223example/Jsr223exampleApplicationTests.java
@@ -3,8 +3,6 @@ package com.example.jsr223example;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 import groovy.lang.GroovyShell;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,164 +10,160 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.time.LocalDateTime;
-import java.util.*;
 
 @SpringBootTest
 class Jsr223exampleApplicationTests {
 
-    private ConnectionObject connectionObject;
-//    private ConnectionObject connectionObject = ConnectionObject.builder()
-//            .url("jdbc:mysql://localhost:3306/demo?useUnicode=true&serverTimezone=GMT%2B8&characterEncoding=UTF-8&useSSL=false")
-//                .username("root")
-//                .password("lzw719")
-//                .driver("com.mysql.cj.jdbc.Driver")
-//                .build();
+  private ConnectionObject connectionObject;
 
+  @BeforeEach
+  void init() {
+    connectionObject = ConnectionObject.builder()
+        .url("jdbc:mysql://localhost:3306/demo?useUnicode=true&serverTimezone=GMT%2B8&characterEncoding=UTF-8&useSSL=false")
+        .username("root")
+        .password("lzw719")
+        .driver("com.mysql.cj.jdbc.Driver")
+        .build();
+  }
 
-    @BeforeEach
-    void init() {
-        connectionObject = ConnectionObject.builder()
-                .url("jdbc:mysql://localhost:3306/demo?useUnicode=true&serverTimezone=GMT%2B8&characterEncoding=UTF-8&useSSL=false")
-                .username("root")
-                .password("lzw719")
-                .driver("com.mysql.cj.jdbc.Driver")
-                .build();
+  @Test
+  void contextLoads() {
+  }
+
+  @Test
+  void testGroovyJSR223() {
+    try {
+      ScriptEngineManager factory = new ScriptEngineManager();
+      ScriptEngine engine = factory.getEngineByName("groovy");
+      String HelloLanguage = "def hello(language,test) {return \"Hello $language,$test\"}";
+      engine.eval(HelloLanguage);
+      Invocable inv = (Invocable) engine;
+      Object[] params = {new String("Groovy"), new String("Abc")};
+      Object result = inv.invokeFunction("hello", params);
+      //assert result.equals("Hello Groovy");
+      System.out.println(result);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    void contextLoads() {
+  @Test
+  void testTime() {
+    for (int i = 0; i < 50; i++) {
+      int i1 = i % 3;
+      if (i1 == 0) {
+        testGroovyJSR223_GroovyClassLoader();
+      } else if (i1 == 1) {
+        testGroovyJSR223_ScriptEngine();
+      } else {
+        testGroovyJSR223_GroovyShell();
+      }
     }
+  }
 
-    @Test
-    void testGroovyJSR223() {
-        try {
-            ScriptEngineManager factory = new ScriptEngineManager();
-            ScriptEngine engine = factory.getEngineByName("groovy");
-            String HelloLanguage = "def hello(language,test) {return \"Hello $language,$test\"}";
-            engine.eval(HelloLanguage);
-            Invocable inv = (Invocable) engine;
-            Object[] params = {new String("Groovy"), new String("Abc")};
-            Object result = inv.invokeFunction("hello", params);
-            //assert result.equals("Hello Groovy");
-            System.out.println(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    void testTime() {
-        for (int i = 0; i < 50; i++) {
-            int i1 = i % 3;
-            if (i1 == 0) {
-                testGroovyJSR223_GroovyClassLoader();
-            } else if (i1 == 1) {
-                testGroovyJSR223_ScriptEngine();
-            } else {
-                testGroovyJSR223_GroovyShell();
-            }
-        }
-    }
-
-    @Test
-    void testGroovyJSR223_GroovyClassLoader() {
-        String str = "import groovy.json.JsonOutput\n" +
-                "\n" +
+  @Test
+  void testGroovyJSR223_GroovyClassLoader() {
+    String str = "import groovy.json.JsonOutput\n" +
+        "\n" +
 //                "class DB{\n" +
-                "    def checkDatabase(connObj){\n" +
-                "        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n" +
-                "        //println sql.connection.catalog\n" +
-                "        def rows = sql.rows(\"select * from t_group\")\n" +
+        "    def checkDatabase(connObj){\n" +
+        "        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n" +
+        "        //println sql.connection.catalog\n" +
+        "        def rows = sql.rows(\"select * from t_group\")\n" +
 //                "        println(rows)\n" +
-//                "        def json = JsonOutput.toJson(rows)\n" +
+        "        def json = JsonOutput.toJson(rows)\n" +
 //                "        println(json)\n" +
 //                "        sql.eachRow(\"select * from group_entity\") {\n" +
 //                "            tp ->\n" +
 //                "                println([tp.id, tp.name])\n" +
 //                "        }\n" +
-                "        sql.close()\n" +
+        "        sql.close()\n" +
+        "        return json\n" +
 //                "    }\n" +
-                "}";
+        "}";
 
-        try {
-            long start = System.currentTimeMillis();
-            GroovyClassLoader loader = new GroovyClassLoader();
-            Class aClass = loader.parseClass(str);
-            GroovyObject groovyObject = (GroovyObject) aClass.newInstance();
-            groovyObject.invokeMethod("checkDatabase", new Object[]{connectionObject});
-            long end = System.currentTimeMillis();
-            System.out.printf("GroovyClassLoader\t->共耗费时间(毫秒): %d\n", end - start);
+    try {
+      long start = System.currentTimeMillis();
+      GroovyClassLoader loader = new GroovyClassLoader();
+      Class aClass = loader.parseClass(str);
+      GroovyObject groovyObject = (GroovyObject) aClass.newInstance();
+      Object result = groovyObject.invokeMethod("checkDatabase", new Object[]{connectionObject});
+      long end = System.currentTimeMillis();
+      System.out.printf("GroovyClassLoader\t->共耗费时间(毫秒): %d\n", end - start);
+      System.out.printf("返回JSON:%s\n", result.toString());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    void testGroovyJSR223_ScriptEngine() {
-        String str = "import groovy.json.JsonOutput\n" +
+  @Test
+  void testGroovyJSR223_ScriptEngine() {
+    String str = "import groovy.json.JsonOutput\n" +
 //                "class DB{\n" +
-                "    def checkDatabase(connObj){\n" +
-                "        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n" +
-                "        //println sql.connection.catalog\n" +
-                "        def rows = sql.rows(\"select * from t_group\")\n" +
+        "    def checkDatabase(connObj){\n" +
+        "        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n" +
+        "        //println sql.connection.catalog\n" +
+        "        def rows = sql.rows(\"select * from t_group\")\n" +
 //                "        println(rows)\n" +
-//                "        def json = JsonOutput.toJson(rows)\n" +
+        "        def json = JsonOutput.toJson(rows)\n" +
 //                "        println(json)\n" +
 //                "        sql.eachRow(\"select * from group_entity\") {\n" +
 //                "            tp ->\n" +
 //                "                println([tp.id, tp.name])\n" +
 //                "        }\n" +
-                "        sql.close()\n" +
+        "        sql.close()\n" +
+        "        return json\n" +
 //                "    }\n" +
-                "}";
+        "}";
 
-        try {
-            long start = System.currentTimeMillis();
-            ScriptEngineManager factory = new ScriptEngineManager();
-            ScriptEngine engine = factory.getEngineByName("groovy");
-            engine.eval(str);
-            Invocable inv = (Invocable) engine;
-            Object result = inv.invokeFunction("checkDatabase", new Object[]{connectionObject});
-            long end = System.currentTimeMillis();
-            System.out.printf("ScriptEngine\t->共耗费时间(毫秒): %d\n", end - start);
+    try {
+      long start = System.currentTimeMillis();
+      ScriptEngineManager factory = new ScriptEngineManager();
+      ScriptEngine engine = factory.getEngineByName("groovy");
+      engine.eval(str);
+      Invocable inv = (Invocable) engine;
+      Object result = inv.invokeFunction("checkDatabase", new Object[]{connectionObject});
+      long end = System.currentTimeMillis();
+      System.out.printf("ScriptEngine\t->共耗费时间(毫秒): %d\n", end - start);
+      System.out.printf("返回JSON:%s\n", result.toString());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @Test
-    void testGroovyJSR223_GroovyShell() {
-        //                "class DB{\n" +
-        //                "        println(rows)\n" +
-        //                "        def json = JsonOutput.toJson(rows)\n" +
-        //                "        println(json)\n" +
-        //                "        sql.eachRow(\"select * from group_entity\") {\n" +
-        //                "            tp ->\n" +
-        //                "                println([tp.id, tp.name])\n" +
-        //                "        }\n" +
-        //                "    }\n" +
-        String str = new StringBuilder()
-                .append("import groovy.json.JsonOutput \n")
-                .append("    def checkDatabase(connObj){\n")
-                .append("        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n")
-                .append("        //println sql.connection.catalog\n")
-                .append("        def rows = sql.rows(\"select * from t_group\")\n")
-                .append("        sql.close()\n")
-                .append("}")
-                .toString();
+  @Test
+  void testGroovyJSR223_GroovyShell() {
+    //                "class DB{\n" +
+    //                "        println(rows)\n" +
+    //                "        def json = JsonOutput.toJson(rows)\n" +
+    //                "        println(json)\n" +
+    //                "        sql.eachRow(\"select * from group_entity\") {\n" +
+    //                "            tp ->\n" +
+    //                "                println([tp.id, tp.name])\n" +
+    //                "        }\n" +
+    //                "    }\n" +
+    String str = new StringBuilder()
+        .append("import groovy.json.JsonOutput \n")
+        .append("    def checkDatabase(connObj){\n")
+        .append("        def sql = groovy.sql.Sql.newInstance(connObj.getUrl(),connObj.getUsername(),connObj.getPassword(),connObj.getDriver())\n")
+        .append("        //println sql.connection.catalog\n")
+        .append("        def rows = sql.rows(\"select * from t_group\")\n")
+        .append("        def json = JsonOutput.toJson(rows)\n")
+        .append("        sql.close()\n")
+        .append("        return json\n")
+        .append("}")
+        .toString();
 
-        try {
-            long start = System.currentTimeMillis();
-            Object result = new GroovyShell().parse(str).invokeMethod("checkDatabase", new Object[]{connectionObject});
-            long end = System.currentTimeMillis();
-            System.out.printf("GroovyShell\t->共耗费时间(毫秒): %d\n", end - start);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    try {
+      long start = System.currentTimeMillis();
+      Object result = new GroovyShell().parse(str).invokeMethod("checkDatabase", new Object[]{connectionObject});
+      long end = System.currentTimeMillis();
+      System.out.printf("GroovyShell\t->共耗费时间(毫秒): %d\n", end - start);
+      System.out.printf("返回JSON:%s\n", result.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
